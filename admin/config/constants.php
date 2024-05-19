6f31556f84b3c9ffa20a81adb1a2e05757386af2
Ashutosh Chaturvedi <acashutosh24@gmail.com>
2024-05-19 23:45:20 +0530

    feat : Blogging Website Initial Commit

---
+++ admin/config/constants.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+define('ROOT_URL', 'http://localhost/blog_website/');
+// define('ROOT_URL', 'https://14ae-2405-201-6011-4e13-bc4c-392f-871-9598.ngrok-free.app/blog_website/');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'blog_website');
